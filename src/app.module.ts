178443a65b2d51f6c14b87aaeec143603e542260
emnaboukhris <e.boukhris@voguelconsulting.com>
2023-02-08 14:54:27 +0100

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3360,
+      port: 3306,
       username: 'root',
       password: '',
       database: 'cvs_nest',
